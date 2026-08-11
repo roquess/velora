a10c3bedc99b3e70bc9ab7458fce3748e58cd0fe
Steve Roques <steve.roques@gmail.com>
2026-08-11 15:49:04 +0200

    chore: scaffold velora OTP app (rast + cowboy deps, release)

---
+++ src/velora_app.erl
@@ -0,0 +1,10 @@
+%%% @private
+-module(velora_app).
+-behaviour(application).
+-export([start/2, stop/1]).
+
+start(_Type, _Args) ->
+    velora_sup:start_link().
+
+stop(_State) ->
+    ok.
---
+++ src/velora_sup.erl
@@ -0,0 +1,11 @@
+%%% @private
+-module(velora_sup).
+-behaviour(supervisor).
+-export([start_link/0, init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    SupFlags = #{strategy => one_for_one, intensity => 10, period => 10},
+    {ok, {SupFlags, []}}.
