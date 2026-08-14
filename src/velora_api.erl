c947cecf9185d9f9ef096f8513084a1b433e5ab0
Steve Roques <steve.roques@gmail.com>
2026-08-14 10:09:19 +0200

    feat: serve the SPA and assets from priv/www via cowboy_static

--- src/velora_api.erl
+++ src/velora_api.erl
@@ -7,13 +7,15 @@ child_spec() ->
     Port = velora_config:http_port(),
     Dispatch = cowboy_router:compile([
         {'_', [
-            {"/health",     velora_api_h, health},
-            {"/cluster",    velora_api_h, cluster},
-            {"/uploads",    velora_upload_h, []},
-            {"/jobs",       velora_api_h, jobs},
-            {"/jobs/:id",   velora_api_h, job},
-            {"/jobs/:id/result", velora_result_h, []},
-            {"/jobs/:id/search", velora_api_h, search}
+            {"/",                 cowboy_static, {priv_file, velora, "www/index.html"}},
+            {"/assets/[...]",     cowboy_static, {priv_dir,  velora, "www/assets"}},
+            {"/health",           velora_api_h, health},
+            {"/cluster",          velora_api_h, cluster},
+            {"/uploads",          velora_upload_h, []},
+            {"/jobs",             velora_api_h, jobs},
+            {"/jobs/:id",         velora_api_h, job},
+            {"/jobs/:id/result",  velora_result_h, []},
+            {"/jobs/:id/search",  velora_api_h, search}
         ]}
     ]),
     ranch:child_spec(velora_http, ranch_tcp, #{socket_opts => [{port, Port}]},
